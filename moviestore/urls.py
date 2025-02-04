8e89b00e2bf60e56c8436fc24682dbce4d72f16c
Lokkit Sanjay Babu Narayanan <143468302+lokkitbabu@users.noreply.github.com>
2025-02-03 21:38:06 -0500

    code refactor, removed polls app and added movies app

--- moviestore/urls.py
+++ moviestore/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import polls
+import movies
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
